bae7520feea51d2551a880f5c1f0596ea52ac73d
maatcha <mattcha54@gmail.com>
2018-07-23 17:31:34 +0200

    it's working gaa!

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,7 @@ require "gossip"
 class GossipProjectApp < Sinatra::Base
  
   	get '/' do
-  	erb :index
+  	erb :index, locals: {gossips: Gossip.all}
 	end
 
 	get '/gossips/new/' do
@@ -11,6 +11,12 @@ class GossipProjectApp < Sinatra::Base
 	end
 
 	post '/gossips/new' do
-  	Gossip.new.save
+  	Gossip.new(params['gossip_author'], params['gossip_content']).save
+  	puts "Salut, je suis dans le serveur"
+  	puts "Ceci est mon super params : #{params}"
+  	puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+  	puts "De la bombe, et du coup ça ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+  	puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+	redirect '/'
 	end
 end
